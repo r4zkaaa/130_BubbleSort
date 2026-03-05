c6c793655dd3b347cd00183bb476b214284a16ff
r4zkaaa <rzkiiahmd11@gmail.com>
2026-03-05 11:41:40 +0700

    Menambahkan display

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -26,3 +26,46 @@ void input() {            //procedur untuk input
         cin >> arr[i];     //input dari pengguna
     }
 }
+
+void bubbleSortArray() { //procedur untuk mengurutkan array dengan metode bubble sort
+    int pass = 1; // step 1
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) { // step 2
+            if (arr[j] > arr[j + 1]) { // step 3
+                int temp;
+                temp = arr[j]; 
+                arr [j] = arr [j + 1];
+                arr [j + 1] = temp;
+        }
+      } 
+      pass = pass + 1; // step 4  
+
+      cout << "\nPass " << pass - 1 << ": "; // number of pass
+        for (int k = 0; k < n; k++) { 
+            cout << arr[k] << " ";  //menampilkan data pada number off pass
+
+     }
+     cout << endl;
+
+    } while (pass <= n - 1); // step 5
+}
+
+void display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Elemen array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout <<  endl;
+    for (int j = 0; j < n; j++) { 
+        cout << arr[j]; //menampilan array 
+        if (j < n - 1) {
+            cout << " --> ";
+      }
+  }
+    cout << endl;
+    cout << endl;
+    cout << "Jumlah pass = " << n - 1 << endl; //menampilkan jumlah dri pass
+    cout << endl;
+    cout << endl;
+}
