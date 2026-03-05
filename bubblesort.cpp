0f96b4ada39d3d07c9e3571bc1e4698453c9e182
r4zkaaa <rzkiiahmd11@gmail.com>
2026-03-05 11:06:21 +0700

    Menambahkan variable global

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -2,3 +2,6 @@
 #include <string>
 using namespace std;
 
+int arr[20]; // Deklarasi variable global array a dengan ukuran 20
+int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen 
+
